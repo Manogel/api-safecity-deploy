e36e59b272e95ea8349aca09370430ad8ca01d5d
Manogel <manoel_ggomes@hotmail.com>
2019-11-29 11:55:15 -0300

    adjustments ChamadoController rith ChamadoNotas

--- src/app/controllers/ChamadoController.js
+++ src/app/controllers/ChamadoController.js
@@ -3,6 +3,8 @@ import Chamado from '../models/Chamado';
 import Categoria from '../models/Categoria';
 import Situacao from '../models/Situacao';
 import Prioridade from '../models/Prioridade';
+import ChamadoNota from '../models/ChamadoNota';
+import Funcionario from '../models/Funcionario';
 
 class ChamadoController {
   async show(req, res) {
@@ -44,13 +46,17 @@ class ChamadoController {
     const { id } = req.params;
     const API_KEY = 'AIzaSyDkUI2f5wkf8MuAV_BgLjfsqbYfIlulJfQ';
     const chamado = await Chamado.findByPk(id, {
-      include: [Situacao, Prioridade, { model: Categoria, as: 'Categoria' }],
+      include: [
+        Situacao,
+        Prioridade,
+        { model: Categoria, as: 'Categoria' },
+        { model: ChamadoNota, as: 'Notas', include: [Funcionario] },
+      ],
     });
     const response = await axios.post(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${chamado.latitude},${chamado.longitude}&key=${API_KEY}`
     );
     const [result, ...rest] = response.data.results;
-    console.log(chamado);
     res.json({ ...chamado.dataValues, endereco: result.formatted_address });
   }
 }
